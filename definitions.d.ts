5eea31a801466d4851c7b61e62b2ee5e99ab07a1
Junaid Bhura <junaid.bhura@travelopia.com>
2023-11-06 11:45:36 +1100

    better typescript support

---
+++ definitions.d.ts
@@ -0,0 +1,8 @@
+export interface TPModalElement extends HTMLElement {
+	open: Function;
+	close: Function;
+}
+
+export interface TPModalCloseElement extends HTMLElement {
+	closeModal: Function;
+}
