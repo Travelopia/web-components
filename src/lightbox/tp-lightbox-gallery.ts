4731d26ba6fa604f394b68b256a101d4dc7a5096
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-02-29 12:15:18 +0530

    Add lightbox initialization on click

--- src/lightbox/tp-lightbox-gallery.ts
+++ src/lightbox/tp-lightbox-gallery.ts
@@ -2,5 +2,79 @@
  * TP Lightbox Gallery
  */
 export class TPLightboxGalleryElement extends HTMLElement {
+	/**
+	 * Properties
+	 */
+	private galleryID: string;
+	private numSlides: number;
+	private currentSlide: number;
 
+	/**
+	 * Constructor
+	 */
+	constructor() {
+		super();
+
+		this.galleryID = this.getAttribute( 'gallery-id' ) ?? '';
+		this.numSlides = 0;
+		this.currentSlide = 0;
+	}
+
+	/**
+	 * Connected callback
+	 */
+	connectedCallback() {
+		const innerElements = [
+			document.createElement( 'tp-lightbox-counter' ),
+			document.createElement( 'tp-lightbox-close' ),
+			document.createElement( 'tp-lightbox-slider' ),
+		];
+
+		if ( '' !== this.galleryID ) {
+			const prevBtn = document.createElement( 'tp-lightbox-nav-button' );
+			const nextBtn = document.createElement( 'tp-lightbox-nav-button' );
+			prevBtn.setAttribute( 'direction', 'prev' );
+			nextBtn.setAttribute( 'direction', 'next' );
+
+			innerElements.push( prevBtn, nextBtn );
+		}
+
+		innerElements.forEach( ( innerElement ) => this.appendChild( innerElement ) );
+	}
+
+	/**
+	 * Adds slide
+	 *
+	 * @param { HTMLElement } slideContent
+	 */
+	addSlide( slideContent: HTMLElement ): number | undefined {
+		if ( ! slideContent ) {
+			return undefined;
+		}
+
+		const slideElement = document.createElement( 'tp-lightbox-slide' );
+		slideElement.appendChild( slideContent );
+
+		this.querySelector( 'tp-lightbox-slider' )?.appendChild( slideElement );
+
+		this.numSlides++;
+
+		return this.numSlides;
+	}
+
+	// Open the lightbox gallery.
+	open( slideIndex: number | undefined ) {
+		if ( ! slideIndex ) {
+			return;
+		}
+
+		this.currentSlide = slideIndex;
+		this.setAttribute( 'current-slide', this.currentSlide.toString() );
+		this.setAttribute( 'open', '' );
+	}
+
+	// Close the lightbox gallery.
+	close() {
+		this.removeAttribute( 'open' );
+	}
 }
--- src/lightbox/tp-lightbox-trigger.ts
+++ src/lightbox/tp-lightbox-trigger.ts
@@ -1,6 +1,75 @@
+import { TPLightboxGalleryElement } from './tp-lightbox-gallery';
+
 /**
  * TP Lightbox Trigger
  */
 export class TPLightboxTriggerElement extends HTMLElement {
+	/**
+	 * Properties
+	 */
+	private galleryID: string;
+	private lightboxContentTemplate: HTMLTemplateElement | null;
+	private slideIndex: number | undefined;
 
+	/**
+	 * Constructor
+	 */
+	constructor() {
+		super();
+
+		this.galleryID = '';
+		this.lightboxContentTemplate = null;
+	}
+
+	/**
+	 * Connected Callback
+	 */
+	connectedCallback(): void {
+		// Initialize fields
+		this.galleryID = this.getAttribute( 'gallery-id' ) ?? '';
+		this.lightboxContentTemplate = this.querySelector( 'template' );
+
+		// Check if there is a template for the content.
+		if ( ! this.lightboxContentTemplate ) {
+			return; // Not found, bail.
+		}
+
+		// Events
+		this.addEventListener( 'click', this.handleClick.bind( this ) );
+	}
+
+	/**
+	 * Event: Handles click event.
+	 */
+	handleClick() {
+		let tpLightboxGallery = document.querySelector( `tp-lightbox-gallery[gallery-id="${ this.galleryID }"]` ) as TPLightboxGalleryElement;
+
+		if ( ! tpLightboxGallery ) {
+			tpLightboxGallery = this.initializeLightbox();
+		}
+
+		tpLightboxGallery.open( this.slideIndex );
+	}
+
+	/**
+	 * Initializes the global TPLightboxElement
+	 */
+	private initializeLightbox(): TPLightboxGalleryElement {
+		let tpLightbox = document.querySelector( 'tp-lightbox' );
+
+		if ( ! tpLightbox ) {
+			tpLightbox = document.createElement( 'tp-lightbox' );
+			document.body.appendChild( tpLightbox );
+		}
+
+		const tpLightboxGallery = document.createElement( 'tp-lightbox-gallery' ) as TPLightboxGalleryElement;
+		tpLightboxGallery.setAttribute( 'gallery-id', this.galleryID );
+		tpLightbox.appendChild( tpLightboxGallery );
+
+		if ( this.lightboxContentTemplate ) {
+			this.slideIndex = tpLightboxGallery.addSlide( this.lightboxContentTemplate?.content.cloneNode( true ) as HTMLElement );
+		}
+
+		return tpLightboxGallery;
+	}
 }
