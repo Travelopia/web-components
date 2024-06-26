848dc86810411eb6ed59322d04204bb73b4fe699
Junaid Bhura <junaid.bhura@travelopia.com>
2024-06-26 17:44:23 +0530

    group trigger

--- src/toggle-attribute/tp-toggle-attribute.ts
+++ src/toggle-attribute/tp-toggle-attribute.ts
@@ -15,41 +15,76 @@ export class TPToggleAttributeElement extends HTMLElement {
 	 */
 	update(): void {
 		// Get trigger element.
-		const triggerSelector: string = this.getAttribute( 'trigger' ) ?? '> *';
+		const triggerSelector: string = this.getAttribute( 'trigger' ) ?? ':scope > *';
 		const triggers: NodeListOf<HTMLElement> | null = this.querySelectorAll( triggerSelector );
 		if ( ! triggers ) {
 			return;
 		}
 
 		// Check for value.
-		if ( this.hasAttribute( 'value' ) ) {
-			triggers.forEach( ( trigger: HTMLElement ) => trigger.addEventListener( 'change', this.valueChanged.bind( this ) ) );
-		}
+		triggers.forEach( ( trigger: HTMLElement ) => trigger.addEventListener( 'change', this.valueChanged.bind( this ) ) );
 	}
 
 	/**
-	 * The value of the target has changed.
+	 * The value of the trigger has changed.
 	 *
 	 * @param {Event} e Change event.
 	 */
 	valueChanged( e: Event ): void {
-		// Check if we're able to detect a value in the target.
-		if ( ! e || ! e.currentTarget || ! ( 'value' in e.currentTarget ) ) {
+		// Check if we're able to detect a value in the trigger.
+		if ( ! e || ! e.currentTarget || ! ( 'value' in e.currentTarget ) || 'string' !== typeof e.currentTarget.value ) {
 			return;
 		}
 
+		// Check if an explicit value is set.
+		if ( this.hasAttribute( 'value' ) ) {
+			this.toggleTargetBasedOnValueAttribute( e.currentTarget.value );
+		} else if ( this.hasAttribute( 'group' ) ) {
+			this.toggleTargetGroupBasedOnTriggerValue( e.currentTarget.value );
+		}
+	}
+
+	/**
+	 * Toggle target based on value set on this component.
+	 *
+	 * @param {string} value Trigger's value.
+	 */
+	toggleTargetBasedOnValueAttribute( value: string = '' ): void {
 		// Get value to listen for.
-		const value: string = this.getAttribute( 'value' ) ?? '';
-		if ( '' === value ) {
+		const valueAttribute: string = this.getAttribute( 'value' ) ?? '';
+		if ( '' === valueAttribute ) {
 			return;
 		}
 
 		// Toggle the target's attribute if the target and trigger have the same value.
-		if ( e.currentTarget.value === value ) {
+		if ( value === valueAttribute ) {
 			this.toggleTargetAttribute( 'on' );
 		} else {
 			this.toggleTargetAttribute( 'off' );
 		}
+	}
+
+	/**
+	 * Toggle group based on value set on trigger.
+	 *
+	 * @param {string} value Trigger's value.
+	 */
+	toggleTargetGroupBasedOnTriggerValue( value: string = '' ): void {
+		// Get group elements.
+		const groupElements: NodeListOf<HTMLElement> | null = this.getGroupElements();
+		if ( ! groupElements ) {
+			return;
+		}
+
+		// Traverse group elements.
+		groupElements.forEach( ( element: HTMLElement ): void => {
+			// Toggle on element attribute if it matches the value.
+			if ( value === element.getAttribute( 'data-toggle-value' ) ) {
+				element.setAttribute( this.getAttributeName(), this.getAttributeValue() );
+			} else {
+				element.removeAttribute( this.getAttributeName() );
+			}
+		} );
 	}
 
 	/**
@@ -85,22 +120,8 @@ export class TPToggleAttributeElement extends HTMLElement {
 			return null;
 		}
 
-		// Set default context.
-		let context: Document | HTMLElement = document;
-
-		// Check for closest ancestor attribute.
-		// If set, the context will be the closest target ancestor.
-		// Note: The target's ancestor must also be this element's ancestor!
-		const closestSelector: string = this.getAttribute( 'closest-ancestor' ) ?? '';
-		if ( '' !== closestSelector ) {
-			const closestContext: HTMLElement | null = this.closest( closestSelector );
-			if ( closestContext ) {
-				context = closestContext;
-			}
-		}
-
 		// Return the target.
-		return context.querySelector( targetSelector );
+		return this.getAncestorContext().querySelector( targetSelector );
 	}
 
 	/**
@@ -125,14 +146,8 @@ export class TPToggleAttributeElement extends HTMLElement {
 	 * Un-toggle the target's group.
 	 */
 	unToggleGroup(): void {
-		// Get group name.
-		const groupName: string | null = this.getAttribute( 'group' );
-		if ( ! groupName ) {
-			return;
-		}
-
 		// Get group elements.
-		const groupElements: NodeListOf<HTMLElement> | null = document.querySelectorAll( `data-toggle-group[${ groupName }]` );
+		const groupElements: NodeListOf<HTMLElement> | null = this.getGroupElements();
 		if ( ! groupElements ) {
 			return;
 		}
@@ -140,5 +155,41 @@ export class TPToggleAttributeElement extends HTMLElement {
 		// Remove attribute from this group.
 		const attributeName: string = this.getAttributeName();
 		groupElements.forEach( ( element: HTMLElement ) => element.removeAttribute( attributeName ) );
+	}
+
+	/**
+	 * Get group elements.
+	 */
+	getGroupElements(): NodeListOf<HTMLElement> | null {
+		// Get group name.
+		const groupName: string | null = this.getAttribute( 'group' );
+		if ( ! groupName ) {
+			return null;
+		}
+
+		// Get group elements.
+		return this.getAncestorContext().querySelectorAll( `[data-toggle-group=${ groupName }]` );
+	}
+
+	/**
+	 * Get ancestor context.
+	 */
+	getAncestorContext(): Document | HTMLElement {
+		// Set default context.
+		let context: Document | HTMLElement = document;
+
+		// Check for closest ancestor attribute.
+		// If set, the context will be the closest target ancestor.
+		// Note: The target's ancestor must also be this element's ancestor!
+		const closestSelector: string = this.getAttribute( 'closest-ancestor' ) ?? '';
+		if ( '' !== closestSelector ) {
+			const closestContext: HTMLElement | null = this.closest( closestSelector );
+			if ( closestContext ) {
+				context = closestContext;
+			}
+		}
+
+		// Return context.
+		return context;
 	}
 }
