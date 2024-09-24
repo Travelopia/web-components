18d7b32cb3e1349e6ceae2ba16065246fe488fd0
harshdeep-gill <harshdeep.gill@travelopia.com>
2024-09-24 09:30:25 +0530

    WP-96 Initialized tooltip

--- src/tooltip/tp-tooltip-popover.ts
+++ src/tooltip/tp-tooltip-popover.ts
@@ -1,5 +1,117 @@
+import { TPTooltipElement } from "./tp-tooltip";
+
 /**
  * TP Tooltip Popover.
  */
 export class TPTooltipPopoverElement extends HTMLElement {
+	/**
+	 * Properties.
+	 */
+	protected trigger: TPTooltipElement | null;
+	protected tooltipWidth: number;
+	protected tooltipArrowHeight: number;
+	protected tooltipGap: number;
+
+	constructor() {
+
+		// Initialize parent.
+		super();
+
+		this.trigger = this.closest( 'tp-tooltip' );
+		this.tooltipWidth = parseInt(this.style.width) || 0;
+		this.tooltipArrowHeight = parseInt(this.getAttribute( 'arrow-height' ) || '8');
+		this.tooltipGap = parseInt(this.getAttribute( 'gap' ) || '10');
+	}
+
+	showTooltip() {
+		this.showPopover();
+		this.positionTooltip();
+	}
+
+	hideTooltip() {
+		this.hidePopover();
+	}
+
+	/**
+	 * Position tooltip according to the position of the trigger element.
+	 * Position is calculated based on the following rules:
+	 * 1. If there is enough space above  the trigger element, tooltip will be placed below the trigger element else it will be above.
+	 * 2. If there is enough space to the right of the trigger element tooltip will be aligned with the parent container of tp-tooltip element
+	 *    the tooltip will be placed to the left of the trigger element.
+	 * 3. Similarly if there is not enough space on right of the trigger element.
+	 */
+	positionTooltip() {
+		// Early return if button not found.
+		if( !this.trigger ) {
+			return;
+		}
+
+		// Getting positions of elements of interest.
+		// Getting position for tooltip popover.
+		const tooltipPopoverPosition = this.getElementPosition( this );
+		console.log(tooltipPopoverPosition);
+
+		// Getting position for tooltip trigger button.
+		const tooltipTriggerPosition = this.getElementPosition( this.trigger );
+
+		// Getting position for tooltip parent element.
+		// It will be used to position tooltip when there is not enough space to the right and left.
+		const parentPosition = this.getElementPosition( this.trigger.parentNode as Element );
+
+		if( tooltipPopoverPosition && tooltipTriggerPosition && parentPosition) {
+			const distanceFromTopWall = (tooltipTriggerPosition.bottom - tooltipTriggerPosition.height);
+
+			// Getting viewport width.
+			const viewportWidth = Math.max( document.documentElement.clientWidth || 0, window.innerWidth || 0 );
+
+			const tooltipTriggerHalfWidth = tooltipTriggerPosition.width / 2;
+			const tooltipPopupHalfWidth = this.tooltipWidth / 2;
+			const marginFromWalls = 0;
+
+			// Setting tooltip text and arrow position in Y direction
+			let textMarginTop;
+			let arrowTopPosition;
+
+			if( tooltipPopoverPosition.height + marginFromWalls > distanceFromTopWall ) {
+				textMarginTop = tooltipTriggerPosition.bottom + this.tooltipGap + this.tooltipArrowHeight;
+				this.style.setProperty( '--border-bottom-color', "#121212" );
+				arrowTopPosition = tooltipTriggerPosition.bottom - this.tooltipArrowHeight + this.tooltipGap;
+			} else {
+				textMarginTop = tooltipTriggerPosition.bottom - tooltipPopoverPosition.height - tooltipTriggerPosition.height - this.tooltipGap - this.tooltipArrowHeight;
+				this.style.setProperty( '--border-top-color', "#121212" );
+				arrowTopPosition = tooltipTriggerPosition.bottom - tooltipTriggerPosition.height - this.tooltipGap - this.tooltipArrowHeight;
+			}
+			console.log(tooltipTriggerPosition.bottom, textMarginTop, arrowTopPosition);
+			
+			this.style.marginTop = `calc(${ textMarginTop }px )`;
+			this.style.setProperty( '--arrow-top-positioning', `${ arrowTopPosition }px` );
+
+
+			// Position tooltip in X direction.
+			let textMarginLeft;
+			const arrowLeftPosition = tooltipTriggerPosition.right - tooltipTriggerHalfWidth;
+
+			if ( viewportWidth - tooltipTriggerPosition.right + tooltipTriggerHalfWidth < tooltipPopupHalfWidth + marginFromWalls ) {
+				// Position of tooltip if there is not enough space to the right.
+				textMarginLeft = parentPosition.right - this.tooltipWidth;
+			} else if ( tooltipTriggerPosition.right - tooltipTriggerHalfWidth < tooltipPopupHalfWidth + marginFromWalls ) {
+				// Position of tooltip if there is not enough space to the left.
+				textMarginLeft = parentPosition.right - parentPosition.width;
+			} else {
+				textMarginLeft = tooltipTriggerPosition.right - tooltipPopupHalfWidth - tooltipTriggerHalfWidth;
+			}
+
+			// Setting location of the tooltip text.
+			this.style.marginLeft = textMarginLeft + 'px';
+			this.style.setProperty( '--arrow-left-positioning', `${ arrowLeftPosition }px` );
+		}
+	}
+
+	/**
+	 * Gets the position of a given element in the viewport.
+	 */
+	getElementPosition( element : Element | null ) {
+		const elementRect = element?.getBoundingClientRect();
+		return elementRect;
+	}
 }
--- src/tooltip/tp-tooltip.ts
+++ src/tooltip/tp-tooltip.ts
@@ -1,5 +1,4 @@
-// import { TPTooltipTriggerElement } from "./tp-tooltip-trigger";
-
+import { TPTooltipPopoverElement } from "./tp-tooltip-popover";
 import { TPTooltipTriggerElement } from "./tp-tooltip-trigger";
 
 /**
@@ -10,34 +9,62 @@ export class TPTooltipElement extends HTMLElement {
 	 * Properties.
 	 */
 	protected uniqueId: string = crypto.randomUUID();
-
+	protected target: TPTooltipPopoverElement | null;
 	/**
 	 * Constructor.
 	 */
 	constructor() {
 		// Initialize parent.
 		super();
-		console.log("tooltip");
+		this.target = this.querySelector( ':scope > tp-tooltip-popover' );
+
 		this.appendTriggerButton();
 		this.setPopoverTarget();
 	}
+
+	/**
+	 * Append a button to the trigger element. This button will be the actual target of mouse events.
+	 * Because popover API only work with Button element as a trigger point.
+	 * We move the slotted content into the button and add listeners to show/hide the tooltip.
+	 * If no trigger element is found, log an error and do nothing.
+	 */
+
 	appendTriggerButton() {
 		const trigger: TPTooltipTriggerElement | null = this.querySelector( 'tp-tooltip-trigger' );
+
+		if ( ! trigger ) {
+			console.error( 'tp-tooltip-trigger not found' );
+			return;
+		}
 		// Move slotted content into the button
 		const button = document.createElement( 'button' );
-		console.log(trigger?.firstElementChild);
 		if ( trigger?.firstElementChild ) {
 			button?.appendChild(trigger.firstElementChild);
 		}
 
 		button.setAttribute( 'popovertarget', this.uniqueId );
 
+		button.addEventListener( 'mouseenter', () => {
+			this.target?.showTooltip();
+		});
+		button.addEventListener( 'mouseleave', () => {
+			this.target?.hideTooltip();
+		});
+
 		trigger?.appendChild( button );
 	}
 
+	/**
+	 * Set the popover target and unique id of the tp-tooltip-popover element.
+	 * The popover target is set to "auto" and the id is set to the uniqueId property.
+	 * If no target element is found, log an error and do nothing.
+	 */
 	setPopoverTarget() {
-		const target = this.querySelector( 'tp-tooltip-popover' );
-		target?.setAttribute( 'popover', "auto" );
-		target?.setAttribute( 'id', this.uniqueId );
+		if( ! this.target ) {
+			console.error( 'tp-tooltip-popover not found' );
+			return;
+		}
+		this.target.setAttribute( 'popover', "auto" );
+		this.target.setAttribute( 'id', this.uniqueId );
 	}
 }
