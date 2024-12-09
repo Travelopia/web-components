7283152a1390e6e78db99b4b86f6dafe817efe8b
Nirav sherasiya <sherasiyanirav@gmail.com>
2024-12-09 15:06:14 +0530

    Add nav items dynamically

--- src/lightbox/tp-lightbox-nav.ts
+++ src/lightbox/tp-lightbox-nav.ts
@@ -1,5 +1,61 @@
 /**
+ * Internal dependencies.
+ */
+import { TPLightboxElement } from './tp-lightbox';
+
+/**
  * TP Lightbox Nav.
  */
 export class TPLightboxNavElement extends HTMLElement {
+	/**
+	 * Properties.
+	 */
+	protected template: HTMLTemplateElement | null = null;
+	protected lightbox : TPLightboxElement | null;
+
+	/**
+	 * Constructor.
+	 */
+	constructor() {
+		// Initialize parent.
+		super();
+
+		// Get the template.
+		this.template = this.querySelector( 'template' );
+		this.lightbox = this.closest( 'tp-lightbox' );
+
+		// Set the template.
+		this.lightbox?.addEventListener( 'template-set', this.setTemplate.bind( this ) );
+	}
+
+	/**
+	 * Set the template.
+	 */
+	setTemplate(): void {
+		// Bail if no template.
+		if ( ! this.template ) {
+			// Exit.
+			return;
+		}
+
+		// Total slides.
+		const totalSlides = Number( this.lightbox?.getAttribute( 'total' ) ?? 0 );
+
+		// Clear the navigation.
+		this.innerHTML = '';
+
+		for ( let i = 0; i < totalSlides; i++ ) {
+			// Clone the template.
+			const clone = document.importNode( this.template.content, true );
+
+			// Set the index.
+			const navItem = clone.querySelector( 'tp-lightbox-nav-item' );
+			if ( navItem ) {
+				navItem.setAttribute( 'index', i.toString() );
+			}
+
+			// Append the clone to the navigation.
+			this.appendChild( clone );
+		}
+	}
 }
