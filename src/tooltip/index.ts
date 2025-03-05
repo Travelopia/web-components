889fe8dc664502f80981b57f0772bf97fa6cefb2
Junaid Bhura <junaid.bhura@travelopia.com>
2025-03-05 16:57:13 +1100

    WP-96 tooltip first commit

---
+++ src/tooltip/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Styles.
+ */
+import './style.scss';
+
+/**
+ * Components.
+ */
+import { TPTooltip } from './tp-tooltip';
+
+/**
+ * Register Components.
+ */
+customElements.define( 'tp-tooltip', TPTooltip );
