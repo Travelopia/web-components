6f19220d9b4e9b68881c34add9861fbfd6dada1e
Junaid Bhura <junaid.bhura@travelopia.com>
2025-03-05 18:13:57 +1100

    WP-96 set the position of the tooltip

--- src/tooltip/index.ts
+++ src/tooltip/index.ts
@@ -8,9 +8,11 @@ import './style.scss';
  */
 import { TPTooltip } from './tp-tooltip';
 import { TPTooltipTrigger } from './tp-tooltip-trigger';
+import { TPTooltipArrow } from './tp-tooltip-arrow';
 
 /**
  * Register Components.
  */
 customElements.define( 'tp-tooltip', TPTooltip );
 customElements.define( 'tp-tooltip-trigger', TPTooltipTrigger );
+customElements.define( 'tp-tooltip-arrow', TPTooltipArrow );
