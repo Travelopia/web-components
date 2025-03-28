7723e46f2b925e6644f25e5369d593ef466d32f6
monikatravelopia <monika.bharti@travelopia.com>
2025-03-28 12:35:57 +0530

    Revert navigation fix

--- src/slider/tp-slider-nav-item.ts
+++ src/slider/tp-slider-nav-item.ts
@@ -53,8 +53,9 @@ export class TPSliderNavItemElement extends HTMLElement {
 
 		// No, find it in the navigation.
 		const slideNav: TPSliderNavElement | null = this.closest( 'tp-slider-nav' );
+		const step = this.slider?.step;
 
 		// Return index of this element considering the step value.
-		return ( Array.from( slideNav?.children ?? [] ).indexOf( this ) + 1 );
+		return ( Array.from( slideNav?.children ?? [] ).indexOf( this ) * ( step ?? 1 ) ) + 1;
 	}
 }
